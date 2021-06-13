29200fd4ebf6f75e54c871b5a07c6bd28d904ae5
michal <michalib646@gmail.com>
2021-06-13 10:11:34 +0300

    kkk

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBNJwPY2cPDimsMnLHBBT4Fre11geRjcd0",
+    authDomain: "moodyrocket-5fefe.firebaseapp.com",
+    databaseURL: "https://moodyrocket-5fefe-default-rtdb.firebaseio.com",
+    projectId: "moodyrocket-5fefe",
+    storageBucket: "moodyrocket-5fefe.appspot.com",
+    messagingSenderId: "956250647109",
+    appId: "1:956250647109:web:2667da9bc71116f339112c",
+    measurementId: "G-EWL4EPD7DK"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBNJwPY2cPDimsMnLHBBT4Fre11geRjcd0",
+    authDomain: "moodyrocket-5fefe.firebaseapp.com",
+    databaseURL: "https://moodyrocket-5fefe-default-rtdb.firebaseio.com",
+    projectId: "moodyrocket-5fefe",
+    storageBucket: "moodyrocket-5fefe.appspot.com",
+    messagingSenderId: "956250647109",
+    appId: "1:956250647109:web:2667da9bc71116f339112c",
+    measurementId: "G-EWL4EPD7DK"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
